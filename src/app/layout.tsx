39225a424539d1da6c6f731f987d7cd89b29725a
Lakshay Kamat <93321969+lakshaykamat@users.noreply.github.com>
2023-07-10 02:53:10 +0530

    Layout modified

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
-"use client";
 import NavBarAndSideBar from "./components/NavBarAndSideBar";
+import Context from "./context/user";
 import "./styles/globals.css";
 import { Inter } from "next/font/google";
-import { useState, useEffect } from "react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -15,35 +14,18 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const [appState, setAppState] = useState<string | null>(null);
 
-  useEffect(() => {
-    setAppState(localStorage.getItem("app"));
-  }, []);
-
-  useEffect(() => {
-    if (appState) {
-      localStorage.setItem("app", appState);
-    }
-  }, [appState]);
   return (
     <html lang="en">
-      <head>
-        <title>Snap Note</title>
-      </head>
       <body className={`${inter.className} bg-very_light`}>
-        {!appState ? (
-          <h1>Loading...</h1>
-        ) : (
-          <>
-            <NavBarAndSideBar setAppState={setAppState} />
+        <Context>
+            <NavBarAndSideBar/>
             <div className="p-4 md:ml-64">
               <main className="px-1 py-2 sm:px-2 mt-14">
                 {children}
               </main>
             </div>
-          </>
-        )}
+        </Context>
       </body>
     </html>
   );
