7a45ac1e7e29958924b61cda53477ef55bdea9e0
Lakshay Kamat <93321969+lakshaykamat@users.noreply.github.com>
2023-07-10 02:51:27 +0530

    Feed page created [D]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,29 @@
+"use client"
+import { useEffect, useState } from "react"
 import Card from "./components/Card"
-import { my_notes } from "./data"
+import { Note } from "./types"
+import { useRouter } from 'next/navigation'
+import { getAllNotes } from "./lib/getAllNotes"
 
 const Feed = () => {
-  const notes = my_notes
+  // const notes = my_notes
+  const [notes,setNotes] = useState<Note[] | null>(null)
+
+  const router = useRouter()
+  useEffect(() => {
+    const fetch = async () => {
+      const res = await getAllNotes() 
+      if(res === "Unauthorized"){
+        //TODO Create a error Page
+        return router.push('/error')
+      }
+      setNotes(res)
+    }
+    fetch()
+    console.log(notes)
+  }, [])
+   
+
   return (
     <div className="flex flex-col items-center max-w-2xl gap-3 mx-auto">
       <div className="flex justify-between w-full mb-2">
@@ -12,14 +33,14 @@ const Feed = () => {
           <option value="">Most Liked</option>
         </select>
       </div>
-      {
+      {!notes ? <h1>Wait...</h1> : 
         notes.map((note) => {
           return <Card
-            key={note.id}
+            key={note._id}
             title={note.title}
             likes={note.likes}
-            description={note.description}
-            date={note.date}
+            content={note.content}
+            date={note.createdAt}
             author={note.author} />
         })
       }
