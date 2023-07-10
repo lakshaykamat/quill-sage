df0740110eec8a045fa11d5ee90f650d0082f67d
Lakshay Kamat <93321969+lakshaykamat@users.noreply.github.com>
2023-07-10 23:28:54 +0530

    Two Layout created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
-import NavBarAndSideBar from "./components/NavBarAndSideBar";
-import Context from "./context/user";
+"use client"
+import { usePathname } from "next/navigation";
 import "./styles/globals.css";
 import { Inter } from "next/font/google";
+import { getLayout } from "./lib/getLayout";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -14,18 +15,12 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-
+  const path = usePathname()
+  const Layout = getLayout(path, children)
   return (
     <html lang="en">
       <body className={`${inter.className} bg-very_light`}>
-        <Context>
-            <NavBarAndSideBar/>
-            <div className="p-4 md:ml-64">
-              <main className="px-1 py-2 sm:px-2 mt-14">
-                {children}
-              </main>
-            </div>
-        </Context>
+        {Layout}
       </body>
     </html>
   );
---
+++ src/app/lib/layout/MainLayout.tsx
@@ -0,0 +1,21 @@
+import NavBarAndSideBar from "@/app/components/NavBarAndSideBar";
+import Context from "@/app/context/user";
+
+export const MainLayout = ({
+    children,
+  }: {
+    children: React.ReactNode
+  }) => {
+    return (
+        <Context>
+        <NavBarAndSideBar/>
+        <div className="p-4 md:ml-64">
+          <main className="px-1 py-2 sm:px-2 mt-14">
+            {children}
+          </main>
+        </div>
+        //Chod yarr mat kar
+    </Context>
+    )
+}
+export default MainLayout
