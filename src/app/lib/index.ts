b0feb6386cb182b88505bbc71ef4465b71cd97d2
Lakshay Kamat <93321969+lakshaykamat@users.noreply.github.com>
2023-07-10 23:28:29 +0530

    Lib folder organized

--- src/app/lib/index.ts
+++ src/app/lib/index.ts
@@ -1,5 +1,86 @@
 import axios from "axios";
 
+export const getUser = async() => {
+    let config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: 'http://localhost:8080/auth/getuser',
+        withCredentials :true,
+        headers: {}
+    };
+
+    try {
+        const response = await axios.request(config);
+        return response.data
+    }
+    catch (error) {
+        console.log(error);
+        throw new Error("Failed to fetch user")
+    }
+}
+
+
+export const getNote = async(id:string) => {
+    let config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: `http://localhost:8080/api/v1/notes/${id}`,
+        withCredentials :true,
+        headers: {}
+    };
+
+    try {
+        const response = await axios.request(config);
+        return response.data
+    }
+    catch (error) {
+        console.log(error);
+        throw new Error("Failed to Fetch Note")
+    }
+}
+
+
+export const getAllFolders = async () => {
+    let config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: 'http://localhost:8080/api/v1/folder/',
+        withCredentials: true,
+        headers: {}
+    };
+
+    try {
+        const response = await axios.request(config);
+        return response.data
+    }
+    catch (error) {
+        console.log(error);
+        throw new Error("Failed to Fetch Folders")
+    }
+}
+
+
+export const getAllNotes = async()=>{
+    let config = {
+      method: 'get',
+      maxBodyLength: Infinity,
+      url: 'http://localhost:8080/api/v1/notes/',
+      withCredentials :true,
+      headers: {}
+    };
+    
+      try {
+        const response = await axios.request(config);
+        return response.data
+      }
+      catch (error:any) {
+        console.log(error.response.data);
+        return error.response.data
+        //throw new Error("Failed to Fetch All Notes")
+      }
+  }
+
+
 export const createFolder = async (name: string) => {
     let data = JSON.stringify({
         "name": name
@@ -16,15 +97,13 @@ export const createFolder = async (name: string) => {
         data: data
     };
 
-
     try {
         const response = await axios.request(config);
         return response.data
     }
     catch (error) {
         console.log(error);
-        throw new Error("Failed to create a folder")
-        // error
+        return error
     }
 
 }
