822eb0b8aaf91f5805d73357e34f8e6383aa58e4
Lakshay Kamat <93321969+lakshaykamat@users.noreply.github.com>
2023-07-15 22:40:10 +0530

    Not found page Created

---
+++ src/app/collections/[name]/not-found.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center px-4 py-8 mx-auto max-w-fit my-28">
+      <div>
+        <h1 className="mb-4 text-3xl font-bold">Not Found</h1>
+        <p className="mb-4">The requested Note could not be found. Please check the URL or go back to the homepage.</p>
+        <Link href="/">
+          <button className='px-3 py-2 font-semibold transition-all rounded-md bg-button hover:bg-primary'>Home</button>
+        </Link>
+      </div>
+    </div>
+  )
+}
+export default NotFound
