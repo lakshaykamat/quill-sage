8d191c8deb23c0d33b860ed58f616a9b2859af53
Lakshay Kamat <93321969+lakshaykamat@users.noreply.github.com>
2023-07-23 19:46:13 +0530

    Add context menu add on folder icons
    context menu add on folder icons on `collections` page for deleting and renaming the folder form page

--- src/app/collections/components/Folder.tsx
+++ src/app/collections/components/Folder.tsx
@@ -1,12 +1,64 @@
 import Link from "next/link";
 import { AiFillFolder } from "react-icons/ai";
+import {
+  useContextMenu,
+  Menu,
+  Item,
+  ItemParams,
+} from "react-contexify";
+import 'react-contexify/ReactContexify.css';
+import { deleteFolder, renameFolder } from "@/app/lib";
 
-const FolderCard = ({name}: {name: string }) => {
-    return (
-      <Link href={`collections/${name}`}  className="flex flex-col items-center p-2 transition-all duration-150 ease-in-out rounded-lg delay-50 hover:bg-slate-200">
-        <AiFillFolder className="w-24 h-24 text-primary md:w-28 md:h-28" />
-        <span className="font-medium">{name}</span>
-      </Link>
-    );
-  };
+
+const MENU_ID = 'blahblah';
+type ItemProps ={
+  key: string;
+}
+
+// Defined just for documentation purpose
+type ItemData = any;
+const FolderCard = ({ name,id }: {name: string,id:string }) => {
+  
+  const { show } = useContextMenu({
+    id: MENU_ID,
+    props: {
+      key:id
+    }
+  });
+  
+  function displayMenu(e: React.MouseEvent) {
+    // pass the item id so the `onClick` on the `Item` has access to it
+    show({event: e, props: { key: id } });
+  }
+
+  const handleItemClick = async({ id,props,}: ItemParams<ItemProps, ItemData>) => {
+    switch (id) {
+      case "del":
+        // @ts-ignore
+        deleteFolder(props?.key)
+        break;
+      case "rename":
+        const name = prompt("Rename Folder:")
+        if(!name) return alert("Enter valid name")
+        // @ts-ignore
+        renameFolder(props?.key,name)
+        break;
+    }
+  }
+  return (
+    <>
+
+      <div id={id} key={id} onContextMenu={displayMenu}>
+        <Link href={`collections/${name}`} className="flex flex-col items-center p-2 transition-all duration-150 ease-in-out rounded-lg delay-50 hover:bg-slate-100">
+          <AiFillFolder className="w-12 h-12 text-slate-600 md:w-24 md:h-24" />
+          <span className="font-medium">{name}</span>
+        </Link>
+      </div>
+      <Menu id={MENU_ID}>
+        <Item id="del" data={id} onClick={handleItemClick}>Delete</Item>
+        <Item id="rename" data={id} onClick={handleItemClick}>Rename</Item>
+      </Menu>
+    </>
+  );
+};
 export default FolderCard
