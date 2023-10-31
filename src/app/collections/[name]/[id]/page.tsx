f5b46e3b21d0b5c659003063e11d3f343070764b
Lakshay Kamat <lakshaykamat2048@gmail.com>
2023-10-31 15:43:12 +0530

    Add Not Found Page
    add note not found page to /collections/[name]/[id]

--- src/app/collections/[name]/[id]/page.tsx
+++ src/app/collections/[name]/[id]/page.tsx
@@ -3,6 +3,7 @@ import { fetchAllTags } from "@/app/utils/api/tags";
 import { fetchNote } from "@/app/utils/api/notes";
 import Editor from "./components/Editor";
 import { useQuery } from '@tanstack/react-query';
+import { notFound } from "next/navigation";
 
 const Page = ({ params }: { params: { id: string } }) => {
   const { id } = params;
@@ -12,6 +13,7 @@ const Page = ({ params }: { params: { id: string } }) => {
 
   
   if(fetchTags.isLoading || fetchNoteData.isLoading ) return <h1>Loading...</h1>
+  if(!fetchNoteData.data) return notFound()
   if(fetchTags.isError || fetchNoteData.isError) return <h1>Error :(</h1>
     return (
       <div className='flex flex-col max-w-6xl gap-1 mx-5 mt-6 xl:mx-auto sm:mt-12'>
