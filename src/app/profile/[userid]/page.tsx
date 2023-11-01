b38a14430e87d7f8767135185cfcacdf8af5421c
Lakshay Kamat <lakshaykamat2048@gmail.com>
2023-11-01 19:21:17 +0530

    profile page imporved
    - Cards are consistent

--- src/app/profile/[userid]/page.tsx
+++ src/app/profile/[userid]/page.tsx
@@ -1,24 +1,24 @@
 "use client"
-import { Note } from "@/app/types";
 import { fetchUser, fetchUserNote } from "@/app/utils/api/user";
 import { useQuery } from "@tanstack/react-query";
 import { notFound } from "next/navigation";
 import Header from "../components/Header";
-import Card from "../../components/Card";
+import ProfileCards from "../components/ProfileCards";
 
 const ProfilePage = ({ params }: { params: { userid: string } }) => {
     const { userid } = params
     const user_details = useQuery({
-        queryKey: ["fetch_user"],
-        queryFn: () => fetchUser(userid)
+        queryKey: ["fetch_user", userid],
+        queryFn: () => fetchUser(userid),
     });
     const user_notes = useQuery({
-        queryKey: ["fetch_user_notes"],
-        queryFn: () => user_details.data && fetchUserNote(user_details.data._id),
-        enabled: user_details.data != null
+        queryKey: ["fetch_user_notes", userid],
+        enabled: user_details.data != null,
+        queryFn: () => user_details.data && fetchUserNote(user_details.data._id)
     });
-    if (user_details.isLoading) return <h1>Loading...</h1>
-    if (user_details.isError) return notFound()
+
+    if (user_details.isLoading || user_notes.isLoading) return <h1>Loading...</h1>
+    if (user_details.isError || user_notes.isError) return notFound()
 
     return (
         <div className="max-w-6xl mx-auto">
@@ -30,28 +30,13 @@ const ProfilePage = ({ params }: { params: { userid: string } }) => {
                 inputBox={null}
                 setInputBox={null}
                 username={user_details.data.username} />
-            <hr />
 
-            {user_notes.isLoading ? <h1>Loading</h1> :
-                <div className="grid grid-cols-3 gap-5 mb-12">
-                    {
-                        user_notes.data.map((note: Note) => {
-                            return (
-                                <Card
-                                    key={note._id}
-                                    note_id={note._id}
-                                    user_id={note.user_id}
-                                    title={note.title}
-                                    likes={note.likes}
-                                    tags={note.tags}
-                                    content={note.content}
-                                    date={note.createdAt}
-                                />
-                            )
-                        })
-                    }
-
-                </div>
+            {
+                user_notes.isLoading ? (
+                    <h1>Loading...</h1>
+                ) : (
+                    <ProfileCards notes={user_notes.data} />
+                )
             }
         </div>
     )
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,12 +1,9 @@
 "use client";
 import { useQuery } from "@tanstack/react-query";
 import { fetchUserNote, updateUser, userDetails } from "../utils/api/user";
-import { Note } from "../types";
 import { FormEvent, useState } from "react";
 import Header from "./components/Header";
-import { ProfileCard } from "./components/ProfileCards";
-import { NotFoundSVG } from "../assets/Illustrations";
-import Card from "../components/Card";
+import ProfileCards from "./components/ProfileCards";
 
 const ProfilePage = () => {
   const [inputBox, setInputBox] = useState({ status: false, text: "" });
@@ -41,29 +38,13 @@ const ProfilePage = () => {
         setInputBox={setInputBox}
         username={user_details.data.username}
       />
-
-      {user_notes.isLoading ? (
-        <h1>Loading</h1>
-      ) : (
-        user_notes.data.length === 0 ? <NotFoundSVG/>
-          :
-          <div className="grid grid-cols-3 gap-5 mb-12">
-            {user_notes.data.map((note: Note) => {
-              return (
-                <Card
-                  key={note._id}
-                  note_id={note._id}
-                  user_id={note.user_id}
-                  title={note.title}
-                  likes={note.likes}
-                  tags={note.tags}
-                  content={note.content}
-                  date={note.createdAt}
-                />
-              );
-            })}
-          </div>
-      )}
+      {
+        user_notes.isLoading ? (
+          <h1>Loading...</h1>
+        ) :(
+          <ProfileCards notes={user_notes.data}/>
+        )
+      }
     </div>
   );
 };
